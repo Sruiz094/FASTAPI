9739c0eb65e5fa1fd3dd6e1677291ecb9db622c0
Sruiz094 <sebastianruizm@utp.edu.co>
2024-03-23 12:22:53 -0500

    configuración base de datos  proyecto

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = "postgresql://curso:sruiz094@localhost/db_clientes"
+#SQLALCHEMY_DATABASE_URL =  "postgresql://db_sr:password@host.docker.internal:5440/DB_USERS_TEST"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 #connect_args={"check_same_thread": False}
